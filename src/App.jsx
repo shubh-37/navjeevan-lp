ad3c62cbb10b7166501cab6b6ed8966913929247
Shubh Arya <shubh.arya37@gmail.com>
2025-01-29 02:06:21 +0530

    added navjeevan banner and also added whatsapp to all store numbers and removed products

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,74 @@
 import { motion } from 'framer-motion';
 import { FaPhone, FaEnvelope, FaClock, FaMapMarkerAlt } from 'react-icons/fa';
 import logo from './assets/logo.png';
-import rice from './assets/rice.webp';
-import tea from './assets/tea.jpg';
-import atta from './assets/atta.webp';
-import oil from './assets/oil.jpg';
 import offer1 from './assets/offer-1.jpeg';
-import offer3 from './assets/offer-3.jpeg';
+import banner from './assets/navjeevan.jpg';
 import offer4 from './assets/offer-4.jpeg';
 import whatsapp from './assets/WhatsApp.svg';
 const App = () => {
   const offers = [
     {
       id: 1,
-      title: 'Weekend Special',
-      description: 'Get 20% off on all groceries',
-      discount: '20% OFF',
+      title: 'Minimum 5% off',
+      description: 'Get 5% off on groceries',
+      discount: '5% OFF',
       image: offer1
     },
-    {
-      id: 3,
-      title: 'Bulk Purchase',
-      description: 'Extra 10% off on purchases above ₹5000',
-      discount: '10% OFF',
-      image: offer3
-    },
+    // {
+    //   id: 3,
+    //   title: 'Bulk Purchase',
+    //   description: 'Extra 10% off on purchases above ₹5000',
+    //   discount: '10% OFF',
+    //   image: offer3
+    // },
     {
       id: 4,
-      title: 'Bulk Purchase',
-      description: 'Extra 10% off on purchases above ₹5000',
-      discount: '10% OFF',
+      title: 'Super Saver Sunday',
+      discount: 'upto 20% OFF',
       image: offer4
     }
   ];
-
-  const featuredProducts = [
-    {
-      id: 1,
-      name: 'Organic Rice',
-      price: 199,
-      image: rice
-    },
-    {
-      id: 2,
-      name: 'Premium Tea',
-      price: 149,
-      image: tea
-    },
-    {
-      id: 3,
-      name: 'Cooking Oil',
-      price: 299,
-      image: oil
-    },
-    {
-      id: 4,
-      name: 'Wheat Flour',
-      price: 249,
-      image: atta
-    }
-  ];
-
   const stores = [
     {
       id: 1,
       location: 'Bahinbai Garden Chowk',
       address: 'RING ROAD, Pratap Nagar, Jalgaon. Pincode: 425001',
-      phone: '+91 7249767089',
+      phone: '7249767089',
       storeTimings: '9AM - 9PM'
     },
     {
       id: 2,
       location: 'Dongargaon Road, Shahada',
       address: 'Dongargaon Road, Shahada City, Shahada - 425409',
-      phone: '+91 7891978920',
+      phone: '7891978920',
       storeTimings: '9AM - 9PM'
     },
     {
       id: 3,
       location: 'Kalinkamata Mandir Chowk',
       address: 'NH No.6, Near Kalinkamata Mandir Chowk, Jalgaon - 425001',
-      phone: '+91 8857058800',
+      phone: '8857058800',
       storeTimings: '9AM - 9PM'
     },
     {
       id: 4,
       location: 'Karvand Naka Chowk, Shirpur',
       address: 'K.G.Compound, Plot No.4, Karvand Naka Chowk, Shirpur - 425405',
-      phone: '+91 8989977171',
+      phone: '8989977171',
       storeTimings: '9AM - 9PM'
     },
     {
       id: 5,
       location: 'Mahabal Chowk',
       address: 'Near Mahabal Chowk, Jalgaon - 425001',
-      phone: '+91 7249677089',
+      phone: '7249677089',
       storeTimings: '9AM - 9PM'
     },
     {
       id: 6,
       location: 'Near Global School, Amalner',
       address: 'Near Global School, Amalner - 425401',
-      phone: '+91 7666869467',
+      phone: '7666869467',
       storeTimings: '9AM - 9PM'
     }
   ];
@@ -112,25 +79,27 @@ const App = () => {
   };
 
   function openWhatsapp(number) {
-    window.open(`https://wa.me/918766860657`, '_blank');
+    window.open(`https://wa.me/${number}`, '_blank');
   }
 
   return (
     <div className="font-sans text-gray-800">
-      {/* Header */}
       <header className="bg-blue-900 text-white py-4">
-        <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
           <motion.img
             src={logo}
             alt="Navjeevan Supershop"
-            className="h-24"
+            className="h-16 md:h-20"
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
           />
-          <div className="text-center md:text-right mt-4 md:mt-0">
-            <button className="flex items-center justify-center md:justify-end cursor-pointer" onClick={openWhatsapp}>
-              <FaPhone className="mr-1" />/<img src={whatsapp} alt="" className="mr-2 ml-1" /> +91 8766860657
+          <div className="text-center md:text-right mt-2 md:mt-0">
+            <button
+              className="flex items-center justify-center md:justify-end cursor-pointer"
+              onClick={() => openWhatsapp(918766860657)}
+            >
+              <FaPhone className="mr-1" />/<img src={whatsapp} alt="" className="mr-2 ml-1 w-5" /> +91 8766860657
             </button>
             <p className="flex items-center justify-center md:justify-end">
               <FaEnvelope className="mr-2" /> contact@navjeevan.com
@@ -138,9 +107,21 @@ const App = () => {
           </div>
         </div>
       </header>
+      {/* Banner Section */}
+      <motion.section
+        className="container mx-auto my-6 px-4"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <img
+          src={banner}
+          alt="Navjeevan Banner"
+          className="w-full max-w-[1200px] mx-auto rounded-lg shadow-md object-cover"
+        />
+      </motion.section>
 
-      {/* Main Content */}
-      <main className="container mx-auto px-4 py-8">
+      <main className="container mx-auto px-4">
         {/* Offers Section */}
         <motion.section
           className="mb-12"
@@ -167,35 +148,6 @@ const App = () => {
           </div>
         </motion.section>
 
-        {/* Featured Products Section */}
-        <motion.section
-          className="mb-12"
-          variants={fadeInVariant}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 0.8, delay: 0.3 }}
-        >
-          <h2 className="text-2xl font-bold text-center text-blue-900 mb-6">Featured Products</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-            {featuredProducts.map((product) => (
-              <motion.div
-                key={product.id}
-                className="p-4 bg-white shadow rounded-lg text-center"
-                whileHover={{ y: -10 }}
-              >
-                <img src={product.image} alt={product.name} className="w-full h-40 object-contain rounded-md mb-4" />
-                <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
-                <p className="text-blue-900 font-bold text-lg mb-4">₹{product.price}</p>
-                {/* <button className="bg-blue-900 text-white py-2 px-4 rounded hover:bg-blue-700">
-                  Add to Cart
-                </button> */}
-              </motion.div>
-            ))}
-          </div>
-          <p className="text-l font-bold text-center text-blue-900 mt-5">And many more such products... Visit store!</p>
-        </motion.section>
-
         {/* Stores Section */}
         <motion.section
           variants={fadeInVariant}
@@ -217,10 +169,10 @@ const App = () => {
                   {store.location}
                 </h3>
                 <p className="text-gray-700 mb-1">{store.address}</p>
-                <p className="text-gray-700 mb-1">
+                <button className="text-gray-700 mb-1 cursor-pointer" onClick={() => openWhatsapp(store.phone)}>
                   <FaPhone className="inline-block mr-2 text-blue-700" />
-                  <a href={`tel:${store.phone}`}>{store.phone}</a>{' '}
-                </p>
+                  {store.phone}
+                </button>
                 <p className="text-gray-500 text-sm flex items-center justify-center">
                   <FaClock className="inline-block mr-2 text-blue-700" />
                   {store.storeTimings}
@@ -237,18 +189,25 @@ const App = () => {
           <div>
             <h3 className="text-lg font-bold mb-2">Follow Us</h3>
             <div className="flex space-x-4">
-              <a href="#" className="hover:underline">
+              <a
+                href="https://www.facebook.com/NavjeevanPlus/"
+                className="hover:underline"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Facebook
               </a>
-              <a href="#" className="hover:underline">
+              <a
+                href="https://www.instagram.com/navjeevanplus/"
+                className="hover:underline"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Instagram
-              </a>
-              <a href="#" className="hover:underline">
-                Twitter
               </a>
             </div>
           </div>
-          <p className="mt-4 md:mt-0">© 2025 Navjeevan Plus Supershop</p>
+          <p className="mt-4 md:mt-0">© Navjeevan Plus Supershop. Since 1964-{new Date().getFullYear()}.</p>
         </div>
       </footer>
     </div>
